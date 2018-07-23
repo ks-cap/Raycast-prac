501e90d362e5c19ddd62b6c78b3581517d1b0418
Ken-S-0114 <g031n071@s.iwate-pu.ac.jp>
2018-07-23 22:19:56 +0900

    first commit

---
+++ Assets/Let's Try Shooter/Scripts/RaycastShoot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaycastShoot : MonoBehaviour {
+
+    public int gunDamage = 1;
+    public float fireRate = .25f;
+    public float weaponRange = 50f;
+
+	void Start () {
+		
+	}
+	
+	
+	void Update () {
+		
+	}
+}
